d1f3fd43d89fc0ce15eccf44fc5ccd57d97e095a
hlkhala022 <hlkhala022@student.wethinkcode.co.za>
2023-12-25 10:37:24 +0200

    word and antonyms functionality

--- target/classes/js/app.js
+++ target/classes/js/app.js
@@ -8,6 +8,66 @@ function lookupWord() {
             method: 'Get',
         };
 
-        document.getElementById('results').innerHTML = <p>Searching for <em>$</em></p>
+        document.getElementById('results').innerHTML = `<p>Searching for <em>${word}</em>...</p>`;
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            let wordData = {
+                word : data[0].word,
+                phonetic: data[0].phonetic,
+                meanings: [],
+                phoneticAudio: data[0].phonetics[0].audio
+            };
+
+            for (let meaning of data[0].meanings) {
+                let meaningData = {
+                    partOfSpeech: meaning.partOfSpeech,
+                    definitions: meaning. definitions
+                };
+                wordData.meanings.push(meaningData);
+            }
+
+            let template = document.getElementById('result-template').innerText;
+            let compiledFunction = Handlebars.compile(template);
+            document.getElementById('results').innerHTML = compiledFunction(wordData);
+            let audioElement = document.querySelector('audio');
+            if (audioElement) {
+                audioElement.addEventListener('play', () => {
+                    audioElement.play();
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+            document.getElementById('results').innerHTML = '<p>Error fetching data. Please check your internet connection or try again later.</p>';
+        });
+    });
+}
+
+function lookupAntonym() {
+    let form = document.getElementById("lookup-form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let data = new FormData(event.target);
+        let word = data.get("word");
+        let options = {
+            method: 'GET',
+        };
+
+        document.getElementById('results').innerHTML = `<p>Searching for <em>${word}</em>...</p>`;
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, options)
+        .then(data => {
+            let wordData = {
+                word: data[0].word,
+                phonetic: data[0].phonetic,
+                antonyms: Object.values(data[0].meanings[1].antonyms),
+                phoneticAudio: data[0].phonetics[0].audio
+            };
+        })
     })
 }
